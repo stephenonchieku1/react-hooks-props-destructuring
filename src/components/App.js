3ee3a61a025896d0d5ba18b9d06685634374a5ef
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-09-26 13:16:27 +0300

    completed ass

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,8 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} 
+      genres={genresArr} />
     </div>
   );
 }
